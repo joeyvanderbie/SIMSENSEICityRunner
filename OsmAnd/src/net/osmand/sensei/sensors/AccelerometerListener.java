4970b738ecb9f3abed8ff460f2c0289aa0df2fa6
joey van der bie <joeyvanderbie@gmail.com>
2014-01-22 16:46:42 +0100

    Accelerometer to database

--- OsmAnd/src/net/osmand/sensei/sensors/AccelerometerListener.java
+++ OsmAnd/src/net/osmand/sensei/sensors/AccelerometerListener.java
@@ -2,11 +2,16 @@ package net.osmand.sensei.sensors;
 
 import java.util.ArrayList;
 
+import net.osmand.plus.R;
 import net.osmand.plus.activities.MapActivity;
+import net.osmand.plus.activities.RunFinishedActivity;
+import net.osmand.sensei.data.AccelData;
+import net.osmand.sensei.db.AccelDataSource;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.util.Log;
+import android.widget.Toast;
 
 public class AccelerometerListener implements SensorEventListener {
 
@@ -15,7 +20,9 @@ public class AccelerometerListener implements SensorEventListener {
     private boolean isActive = false;
     private double samplingRate = 0.0;
     private MapActivity accelerometerTest;
-    private ArrayList<SensorEvent> samples;
+    private ArrayList<AccelData> samples;
+    private AccelDataSource ads;
+    private int run_id;
     
     public AccelerometerListener(MapActivity accelerometerTest) {
         this.accelerometerTest = accelerometerTest;
@@ -25,15 +32,18 @@ public class AccelerometerListener implements SensorEventListener {
         return samplingRate;
     }
     
-    public ArrayList<SensorEvent> getSamples(){
+    public ArrayList<AccelData> getSamples(){
     	return samples;
     }
     
-    public void startRecording() {
+    public void startRecording(int run_id) {
         startTime = System.currentTimeMillis();
         numSamples = 0;
         isActive = true;
-        this.samples = new ArrayList<SensorEvent>();
+        this.samples = new ArrayList<AccelData>();
+        ads = new AccelDataSource(accelerometerTest);
+        this.run_id = run_id;
+		
     }
     
     public boolean isActive() {
@@ -49,14 +59,34 @@ public class AccelerometerListener implements SensorEventListener {
         if (isActive) {
             numSamples++;
             long now = System.currentTimeMillis();
-            if ((now - startTime) % 5000 == 0) {
+            if (numSamples % 1000 == 0) {
                 samplingRate = numSamples / ((now - startTime) / 1000.0);                
               //  isActive = false;
+                startTime = now;
+                numSamples = 0;
                 
                 //accelerometerTest.displayRates();
                 Log.d("AcceleromterTest", "displayrate: "+samplingRate);
+                
+                //add samples to database
+                ads.open();
+               // ads.addAccelDataList(samples, 0, run_id);
+                ads.addAccelDataListFast(samples, 0, run_id);
+    			ads.close();
+                samples = new ArrayList<AccelData>();
+                
+                accelerometerTest.showToast(R.string.msg_sent_data, " Add 1000 accelerometer samplings to DB");
             }
-            samples.add(event);
+            
+            samples.add(new AccelData(event.timestamp, event.values[0], event.values[1], event.values[2], run_id));
         }
+    }
+    
+    public void submitLastSensorData(){
+    	ads.open();
+        // ads.addAccelDataList(samples, 0, run_id);
+         ads.addAccelDataListFast(samples, 0, run_id);
+			ads.close();
+         samples = new ArrayList<AccelData>();
     }
 }
